045078b57bd94376fc9a1177a7a7166ff964b91d
mcash <mcash@proforma.com>
2023-02-28 13:07:08 -0500

    hmmmm

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-let app = angular.module("mainApp", ['ngRoute']);
+let app = angular.module('mainApp', ['ngRoute']);
 
 app.config(function ($routeProvider) {
     $routeProvider
-        .when("/", {
-            templateUrl: "index.html"
+        .when('/', {
+            templateUrl: 'index.html'
         })
-        .when("/a", {
-            templateUrl: "index.html"
+        .when('/a', {
+            templateUrl: 'index.html'
         })
-        .when("/b", {
-            templateUrl: "landing.html"
+        .when('/b', {
+            templateUrl: 'landing.html'
         })
-        .when("/c", {
-            templateUrl: "addpage.html"
+        .when('/c', {
+            templateUrl: 'addpage.html'
         });
 });
 
 app.controller('mainCtrl', function ($scope, $http) {
-
-    netlifyIdentity.on('init', () => {
+    //
+    $scope.netlifyIdentity.on('init', () => {
         initUser = netlifyIdentity.currentUser();
         console.log(initUser);
     });
 
-    netlifyIdentity.on('login', () => {
+    $scope.netlifyIdentity.on('login', () => {
         if (initUser == null) {
             window.location.replace('#!b');
         }
         netlifyIdentity.close();
     });
 
-    netlifyIdentity.on('logout', () => {
+    $scope.netlifyIdentity.on('logout', () => {
         netlifyIdentity.close();
         window.location.replace('/');
     })
