8f8aee1513504815e02fc55aaa0744ef4cad871c
mcash <mcash@proforma.com>
2023-02-28 11:33:51 -0500

    another

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.controller('mainCtrl', function ($scope, $http) {
 
     netlifyIdentity.on('login', () => {
         if (initUser == null) {
-            window.location.replace('#!b');
+            window.location.replace('landing');
         }
         netlifyIdentity.close();
     });
